786b08e7979dabec7e187ea458a1b8da8d70e76e
borbrudar <41475472+borbrudar@users.noreply.github.com>
2021-11-09 21:19:46 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bor/source/Life
-# Build directory: /home/bor/source/Life/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/SFML")
